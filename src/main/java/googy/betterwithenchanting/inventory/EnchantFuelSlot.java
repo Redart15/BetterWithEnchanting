150293c107e7e221b0d72522fcad2351b7fc8c5e
Redart15 <redart15@users.noreply.github.com>
2026-04-13 17:35:25 +0200

    slot now uses the outline texture rather than having them backed in

--- src/main/java/googy/betterwithenchanting/inventory/EnchantFuelSlot.java
+++ src/main/java/googy/betterwithenchanting/inventory/EnchantFuelSlot.java
@@ -5,7 +5,11 @@ import net.minecraft.core.item.Items;
 import net.minecraft.core.player.inventory.container.Container;
 import net.minecraft.core.player.inventory.slot.Slot;
 
+import static googy.betterwithenchanting.BetterWithEnchanting.MOD_ID;
+
 public class EnchantFuelSlot extends Slot {
+	public static final String FUEL = MOD_ID + ":item/lapiz";
+
 	public EnchantFuelSlot(Container inventory, int id, int x, int y) {
 		super(inventory, id, x, y);
 	}
@@ -13,5 +17,10 @@ public class EnchantFuelSlot extends Slot {
 	@Override
 	public boolean mayPlace(ItemStack itemstack) {
 		return itemstack != null && itemstack.getItem().id == Items.DYE.id && itemstack.getMetadata() == 4;
+	}
+
+	@Override
+	public String getItemIcon() {
+		return FUEL;
 	}
 }
