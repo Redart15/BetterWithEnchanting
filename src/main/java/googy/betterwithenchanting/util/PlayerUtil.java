ccf9437328fffd1034f4f5d152ac1c2aebfe6f17
Redart15 <redart15@users.noreply.github.com>
2026-08-04 02:38:18 +0200

    fix score and bump version

--- src/main/java/googy/betterwithenchanting/util/PlayerUtil.java
+++ src/main/java/googy/betterwithenchanting/util/PlayerUtil.java
@@ -6,27 +6,23 @@ public class PlayerUtil {
 	private PlayerUtil(){}
 
 	public static int addScore(Player player, int value) {
-		boolean isNeg = (value >> 31 & 1) == 1;
-		if (isNeg) {
-			return PlayerUtil.addScore(player, Integer.MIN_VALUE, value);
-		} else {
-			return PlayerUtil.addScore(player, Integer.MAX_VALUE ,value);
+		int currentScore = player.getScore();
+		if(currentScore > Integer.MAX_VALUE - value){
+			player.score = Integer.MAX_VALUE;
+			return Integer.MAX_VALUE - currentScore;
 		}
+		player.score += value;
+		return value;
 	}
 
 	public static int subScore(Player player, int value) {
-		return PlayerUtil.addScore(player, value);
-	}
-
-	private static int addScore(Player player, int boarder, int value) {
 		int currentScore = player.getScore();
-		if(boarder - value > currentScore){
-			player.score += value;
-			return value;
-		}else{
-			player.score = boarder;
-			return boarder - currentScore;
+		if(currentScore < Integer.MIN_VALUE + value){
+			player.score = Integer.MIN_VALUE;
+			return currentScore - Integer.MIN_VALUE + 1;
 		}
+		player.score -= value;
+		return value;
 	}
 
 	public static void setScore(Player player, int value){
